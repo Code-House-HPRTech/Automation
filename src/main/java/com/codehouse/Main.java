b1b158023ea9e6cf2c64b989bcb016880ac41d99
Prakash Jangir <prakash_jangir@epam.com>
2024-09-27 16:58:31 +0530

    Added folder creation part

--- src/main/java/com/codehouse/Main.java
+++ src/main/java/com/codehouse/Main.java
@@ -4,6 +4,8 @@ import com.codehouse.contants.Constant;
 import com.codehouse.steps.WordpressService;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,17 +22,22 @@ public class Main {
         SITE_DIR_MAP.put("", "");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*
-        Operation Type:
-        1. DOWNLOAD_DATA_AND_MEDIA
-        2. DOWNLOAD_MY_MEDIA_DATA
-        3. UPDATE_POST_WITH_CTM_AND_PREPARE_CSV
+            Operation Type:
+                1. DOWNLOAD_DATA_AND_MEDIA
+                2. DOWNLOAD_MY_MEDIA_DATA
+                3. UPDATE_POST_WITH_CTM_AND_PREPARE_CSV
          */
         String operationType = Constant.OperationType._1_DOWNLOAD_DATA_AND_MEDIA;
 
         SITE_DIR_MAP.forEach((siteUrl, folderName) -> {
             try {
+                // Create Directory if not exist
+                Path folder = Path.of(String.format(Constant.WP_DATA_BASE_PATH, folderName));
+                Files.createDirectories(folder);
+
+                // Call actual Service
                 WordpressService.downloadAndPrepareData(siteUrl, MY_SITE_URL, folderName, operationType);
             } catch (IOException e) {
                 throw new RuntimeException(e);
